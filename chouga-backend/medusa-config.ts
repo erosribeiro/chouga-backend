425549137b4f94252c4a3eaf5459fef7734043b5
Eros Ribeiro <erosr@github.com>
2026-04-19 15:58:07 -0300

    fix: disable admin in prod to reduce RAM usage

--- chouga-backend/medusa-config.ts
+++ chouga-backend/medusa-config.ts
@@ -2,15 +2,20 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+      storeCors: process.env.STORE_CORS || 'http://localhost:8000',
+      adminCors: isProduction ? '' : process.env.ADMIN_CORS || 'http://localhost:5173,http://localhost:9000',
+      authCors: process.env.AUTH_CORS || 'http://localhost:5173,http://localhost:9000,http://localhost:8000',
+      jwtSecret: process.env.JWT_SECRET,
+      cookieSecret: process.env.COOKIE_SECRET,
     }
+  },
+  admin: {
+    disabled: isProduction && process.env.DISABLE_ADMIN === 'true',
   }
 })
